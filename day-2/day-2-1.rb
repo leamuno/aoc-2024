4cae2a0ff41df109a98783b55e41398e57fa4e85
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-12-03 10:22:07 +0900

    advent of code day 1 and 2

---
+++ day-2/day-2-1.rb
@@ -0,0 +1,18 @@
+count = 0
+
+def is_safe(report)
+  return false if report != report.sort && report != report.sort.reverse
+
+  report.each_cons(2).all? { |a, b| (1..3).include?((b - a).abs) }
+end
+
+File.open("./input.txt").each do |line|
+  report = line.split(" ").map(&:to_i)
+
+  count += 1 if is_safe(report)
+end
+
+
+
+
+puts count
---
+++ day-2/day-2-2.rb
@@ -0,0 +1,26 @@
+count = 0
+
+def is_safe(report)
+  return false if report != report.sort && report != report.sort.reverse
+
+  report.each_cons(2).all? { |a, b| (1..3).include?((b - a).abs) }
+end
+
+def use_dampener(report)
+  return true if is_safe(report)
+
+  report.each_with_index do |_, index|
+    modified_report = report[0...index] + report[index + 1..-1]
+    return true if is_safe(modified_report)
+  end
+
+  false
+end
+
+File.open("./input.txt").each do |line|
+  report = line.split(" ").map(&:to_i)
+
+  count += 1 if use_dampener(report)
+end
+
+puts count
