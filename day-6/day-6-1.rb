7576544d68b48e7e3c4702a8360d378ccd411248
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-12-06 17:23:02 +0900

    completes aoc day 6

--- day-6/day-6-1.rb
+++ day-6/day-6-1.rb
@@ -1,3 +1,43 @@
-data = File.read("day-5/input.txt")
+require 'set'
 
-puts data
+DIR = { ">" => [0, 1], "<" => [0, -1], "v" => [1, 0], "^" => [-1, 0] }
+TURN = { ">" => "v", "<" => "^", "v" => "<", "^" => ">" }
+
+def follow_path(grid, pos)
+  visited = Set.new
+  char = grid[pos]
+
+  while grid[pos] != nil
+    visited << [pos, char]
+
+    next_pos = DIR[char].zip(pos).map(&:sum)
+
+    if grid[next_pos] == "#"
+      char = TURN[char]
+      next_pos = DIR[char].zip(pos).map(&:sum)
+    end
+
+    pos = next_pos
+  end
+
+  visited.map(&:first).uniq
+end
+
+def results(data)
+  start_pos, = data.select { |_k, v| DIR.keys.include?(v) }.first
+  follow_path(data, start_pos).size
+end
+
+def process_dataset(set)
+  grid = {}
+  set.each_with_index do |line, y|
+    line.chars.each_with_index do |c, x|
+      grid[[y, x]] = c
+    end
+  end
+  grid
+end
+
+dataset = File.read("day-6/input.txt").split("\n")
+data = process_dataset(dataset)
+puts results(data)
--- day-6/day-6-2.rb
+++ day-6/day-6-2.rb
@@ -1,3 +1,49 @@
-data = File.read("day-5/input.txt")
+require 'set'
 
-puts data
+DIR = { ">" => [0, 1], "<" => [0, -1], "v" => [1, 0], "^" => [-1, 0] }
+TURN = { ">" => "v", "<" => "^", "v" => "<", "^" => ">" }
+
+def follow_path(grid, pos, obstructed_pos = nil)
+  visited = Set.new
+  char = grid[pos]
+
+  while grid[pos] != nil
+    return false if visited.include?([pos, char])
+    visited << [pos, char]
+
+    next_pos = DIR[char].zip(pos).map(&:sum)
+    while grid[next_pos] == "#" || next_pos == obstructed_pos
+      char = TURN[char]
+      next_pos = DIR[char].zip(pos).map(&:sum)
+    end
+
+    pos = next_pos
+  end
+
+  visited.map(&:first).uniq
+end
+
+def results(data)
+  start_pos, = data.select { |_k, v| DIR.keys.include?(v) }.first
+
+  follow_path(data, start_pos.dup).count do |test_pos|
+    next false if test_pos == start_pos
+    follow_path(data, start_pos, test_pos) == false
+  end
+end
+
+def process_dataset(set)
+  grid = {}
+  set.each_with_index do |line, y|
+    line.chars.each_with_index do |c, x|
+      grid[[y, x]] = c
+    end
+  end
+  grid
+end
+
+dataset = File.read("day-6/input.txt").split("\n")
+
+data = process_dataset(dataset)
+
+puts results(data)
