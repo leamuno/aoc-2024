9b80432f210fcf04dcb321144b7bbdd64cb6d403
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-12-11 17:44:55 +0900

    Solves day 7 and 8 of aoc

--- day-7/day-7-2.rb
+++ day-7/day-7-2.rb
@@ -1,3 +1,48 @@
+def evaluate_expression(numbers, operators)
+  result = numbers[0]
+  operators.each_with_index do |op, i|
+    if op == '+'
+      result += numbers[i + 1]
+    elsif op == '*'
+      result *= numbers[i + 1]
+    elsif op == '||'
+      result = "#{result}#{numbers[i+1]}".to_i
+    end
+  end
+  result
+end
+
+def parse_input(input_data)
+  equations = []
+  input_data.strip.split("\n").each do |line|
+    test_value, numbers = line.split(":")
+    test_value = test_value.strip.to_i
+    numbers = numbers.strip.split.map(&:to_i)
+    equations << [test_value, numbers]
+  end
+  equations
+end
+
+def find_valid_equations(equations)
+  total_calibration_result = 0
+
+  equations.each do |test_value, numbers|
+    n = numbers.length - 1
+    valid = false
+
+    ["+", "*", "||"].repeated_permutation(n).each do |operators|
+      if evaluate_expression(numbers, operators) == test_value
+        valid = true
+        break
+      end
+    end
+
+    total_calibration_result += test_value if valid
+  end
+
+  total_calibration_result
+end
+
 data = File.read("day-7/input.txt")
 
-puts data
+puts find_valid_equations(parse_input(data))
--- day-8/day-8-2.rb
+++ day-8/day-8-2.rb
@@ -1,3 +1,45 @@
-data = File.read("day-8/input.txt")
+require 'set'
 
-puts data
+def antenna_positions(grid)
+  antennas = Hash.new { |h, k| h[k] = [] }
+  grid.each_with_index do |row, y|
+    row.each_with_index do |cell, x|
+      antennas[cell] << [x, y] if cell != '.'
+    end
+  end
+  antennas
+end
+
+def find_antinodes(antennas, x_range, y_range, resonants = (1..1))
+  antinodes = Set.new
+  antennas.each do |_, locations|
+    locations.combination(2) do |(x1, y1), (x2, y2)|
+      resonants.each do |resonant|
+        a1x = x1 + resonant * (x1 - x2)
+        a1y = y1 + resonant * (y1 - y2)
+        break unless x_range.include?(a1x) and y_range.include?(a1y)
+        antinodes << [a1x, a1y]
+      end
+
+      resonants.each do |resonant|
+        a2x = x2 + resonant * (x2 - x1)
+        a2y = y2 + resonant * (y2 - y1)
+        break unless x_range.include?(a2x) and y_range.include?(a2y)
+        antinodes << [a2x, a2y]
+      end
+    end
+  end
+  antinodes
+end
+
+grid = []
+
+input = File.read("day-8/input.txt")
+input.each_line do |line|
+  grid << line.strip.chars
+end
+
+antennas = antenna_positions(grid)
+
+antinodes = find_antinodes(antennas, 0...grid[0].size, 0...grid.size, 0..)
+puts antinodes.size
