0e42a25061b7efe7f5c4dc7f8505df95febd0afb
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-12-12 19:52:42 +0900

    solves half of aoc day 9

--- day-9/day-9-1.rb
+++ day-9/day-9-1.rb
@@ -1,3 +1,60 @@
-data = File.read("day-9/input.txt")
+data = File.open("day-9/input.txt")
 
-puts data
+arr = []
+
+
+def parse(data, arr)
+  ind = 0
+  for num in 0..data.size - 2 do
+    current_num = data.sysread(1).to_i
+    if num % 2 == 0
+      current_num.times do
+        arr << ind
+      end
+      ind += 1
+    else
+      current_num.times do
+        arr << "."
+      end
+    end
+  end
+end
+
+parse(data, arr)
+
+p arr.join
+
+def compact(arr)
+  ind = 0
+  l_ind = arr.size - 1
+
+  while ind < l_ind
+    if arr[ind] == "."
+      arr[ind] = arr[l_ind]
+      arr[l_ind] = "."
+      l_ind -= 1
+      while arr[l_ind] == "."
+        l_ind -= 1
+        break if ind >= l_ind
+      end
+    end
+    ind += 1
+  end
+end
+
+compact(arr)
+
+p arr.join
+
+def checksum(arr)
+  total = 0
+  arr.each_with_index do |num, ind|
+    break if num == "."
+    total += num.to_i * ind
+  end
+  return total
+end
+
+puts checksum(arr)
+
+data.close
--- day-9/day-9-2.rb
+++ day-9/day-9-2.rb
@@ -1,3 +1,80 @@
-data = File.read("day-9/input.txt")
+data = File.open("day-9/test.txt")
 
-puts data
+arr = []
+
+
+def parse(data, arr)
+  ind = 0
+  for num in 0..data.size - 2 do
+    current_num = data.sysread(1).to_i
+    if num % 2 == 0
+      current_num.times do
+        arr << ind
+      end
+      ind += 1
+    else
+      current_num.times do
+        arr << "."
+      end
+    end
+  end
+end
+
+parse(data, arr)
+
+def compact(arr, endp)
+  startp = endp - 1
+  val = arr[endp]
+
+  while val == arr[startp]
+    break if startp == 0 || val != arr[startp]
+    startp -= 1
+  end
+  len = endp - startp
+  count = 0
+  arr.each_with_index do |num, ind|
+    break if ind == startp
+    if num != "."
+      count = 0
+    else
+      count += 1
+      if count == len
+        s = ind - len
+        return [arr[0..s] + arr[startp+1..endp] + arr[ind+1..startp] + Array.new(count) {|i| "." } + arr[endp+1..], startp]
+      end
+    end
+  end
+  return [arr, startp]
+end
+
+endp = arr.size - 1
+val = Float::INFINITY
+help = 0
+while val != 0
+  current_val = arr[endp]
+  if current_val != "." && val > current_val.to_i
+    val = arr[endp].to_i
+    results = compact(arr, endp)
+    arr = results[0]
+    endp = results[1]
+    puts "loop #{help} val #{val}"
+  else
+    endp -= 1
+  end
+  help += 1
+end
+
+p arr
+
+def checksum(arr)
+  total = 0
+  arr.each_with_index do |num, ind|
+    next if num == "."
+    total += num.to_i * ind
+  end
+  return total
+end
+
+puts checksum(arr)
+
+data.close
